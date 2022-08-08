d07258f09f2e263947c77b9d7c1fe72c632c2d28
Alize Nguyen <alizenguyen@gmail.com>
2022-08-08 17:06:44 -0500

    Introduce error after ignore

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Deck from './src/components/Deck.js'
 
 function App() {
+    z="hi";
+    let z;
   return (
     <div className="App">
       <div className="App-container">
