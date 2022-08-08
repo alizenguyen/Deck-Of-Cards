31e5f0e5b1e7829b610f3d8f39ba30a1bc729e16
Alize Nguyen <alizenguyen@gmail.com>
2022-08-08 16:53:17 -0500

    fix error

--- App.js
+++ App.js
@@ -2,8 +2,6 @@ import './App.css';
 import Deck from './src/components/Deck.js'
 
 function App() {
-    z = 'hi';
-    let z;
   return (
     <div className="App">
       <div className="App-container">
